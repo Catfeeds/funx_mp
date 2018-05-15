caa75d0010e0ecac8c71b87a5a9d6c83afa5b688
zhangjunhu <401967974@qq.com>
2018-05-15 19:09:16 +0800

    '创建web端用户控制器'

---
+++ application/controllers/account/Customer.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author:      zjh<401967974@qq.com>
+ * Date:        2018/5/15 0015
+ * Time:        19:05
+ * Describe:    用户
+ */
+class Customer extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
