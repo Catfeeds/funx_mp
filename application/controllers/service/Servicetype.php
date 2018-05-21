f262087f839da62319b429aab39e62fda4d7023e
dowell_funxdata <apsondu@gmail.com>
2018-05-21 15:19:56 +0800

    wws-服务列表

--- application/controllers/service/Servicetype.php
+++ application/controllers/service/Servicetype.php
@@ -20,11 +20,13 @@ class Servicetype extends MY_Controller
      */
     public function servicetype(){
 
-        $post   = $this->input->post(NULL,TRUE);
-        $where  = [];
-        isset($post['id'])?$where['id']=intval($post['id']):$where=[];
+
         $filed  = ['id','name','image_url'];
-        $type   = Servicetypemodel::where($where)->orderBy('id','desc')->get($filed)->toArray();
+        $type   = Servicetypemodel::orderBy('id','desc')->get($filed)->toArray();
+
+        foreach ($type as $key=>$value){
+            $type[$key]['image_url'] = $this->fullAliossUrl($value['image_url']);
+        }
         $this->api_res(0,['list'=>$type]);
     }
 
