cb02a8dd56b06af812e7f40e97fcd63845408450
zhangjunhu <401967974@qq.com>
2018-05-29 18:42:29 +0800

    '支付回调'

---
+++ application/models/Storepaymodel.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author:      zjh<401967974@qq.com>
+ * Date:        2018/5/29 0029
+ * Time:        18:32
+ * Describe:
+ *
+ */
+class Storepaymodel extends Basemodel{
+
+    protected $table    = 'web_store_pay';
+}
