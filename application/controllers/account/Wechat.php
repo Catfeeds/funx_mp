3339b816b5b1187732a0e3e4ddfd2dbae2353204
zhangjunhu <401967974@qq.com>
2018-06-07 16:42:46 +0800

    'nothing'

--- application/controllers/account/Wechat.php
+++ application/controllers/account/Wechat.php
@@ -33,7 +33,7 @@ class Wechat extends MY_Controller
         $user   = $this->httpCurl($url,'get','json');
         if(array_key_exists('errcode',$user))
         {
-            log_message('error',$user['errmsg']);
+            log_message('error','请求access_token'.$user['errmsg']);
             $this->api_res(1006);
             return false;
         }
@@ -44,7 +44,12 @@ class Wechat extends MY_Controller
         $unionid        = $user['unionid'];
         $info_url   = 'https://api.weixin.qq.com/sns/userinfo?access_token'.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $user_info  = $this->httpCurl($info_url,'get','json');
-        log_message('error','---->'.json_encode($user_info));
+        if(array_key_exists('errcode',$user_info)){
+            log_message('error','请求info:'.$user_info['errmsg']);
+            $this->api_res(1006);
+            return false;
+        }
+        log_message('error','----->'.json_encode($user_info));
         $nickname   = $user_info['nickname'];
         $gender     = $user_info['sex'];
         $province   = $user_info['province'];
