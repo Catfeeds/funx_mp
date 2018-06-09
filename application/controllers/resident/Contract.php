8dfc6617d1d08aa52bd07f9a8da80962d7cab34a
dowell_funxdata <apsondu@gmail.com>
2018-06-09 20:19:25 +0800

    '合同'

--- application/controllers/resident/Contract.php
+++ application/controllers/resident/Contract.php
@@ -81,6 +81,140 @@ class Contract extends MY_Controller
         $this->api_res(0,['contract'=>$contract]);
     }
     /**
+     * 合同确认页面-确认签约
+     */
+    public  function confirm(){
+
+        $input  = $this->input->post(null,true);
+        log_message('error',json_encode($input));
+        $resident_id    = intval(strip_tags($input['resident_id']));
+        $phone          = trim(strip_tags($input['phone']));
+//        $code           = trim(strip_tags($input['code']));
+        //验证短信验证码
+        $this->load->library('m_redis');
+        if(!$this->m_redis->verifyResidentPhoneCode($input['phone'],$input['code'])){
+            $this->api_res(10007);
+            return;
+        }
+        $this->load->model('residentmodel');
+        $resident   = Residentmodel::find($resident_id);
+        if(!$resident){
+            $this->api_res(1007);
+            return;
+        }
+        if($resident->phone!=$phone){
+            $this->api_res(10010);
+            return;
+        }
+
+        //验证住户的uxid是不是当前ID
+        $this->checkUser($resident->uxid);
+        $this->load->model('roomunionmodel');
+        $room   = $resident->roomunion;
+        if($room->status!=Roomunionmodel::STATE_OCCUPIED){
+            $this->api_res(10014);
+            return;
+        }
+
+//      判断住户合同是否已经归档，有已经归档的合同 就结束
+        $this->load->model('contractmodel');
+//        $has_contract = $resident->contract()->where('status', Contractmodel::STATUS_ARCHIVED);
+        $has_contract = $resident->contract();
+        if ($has_contract->exists()) {
+            $this->api_res(10015);
+            return;
+        }
+
+        //判断门店的合同类型选择调用哪个合同流程
+        $this->load->model('storemodel');
+        $contract   = $resident->contract;
+        $contract_type  = $room->store->contract_type;
+
+        //测试使用
+        $data   = $this->test();
+        /*if(Storemodel::C_TYPE_NORMAL==$contract_type){
+            if(empty($contract)){
+                //生成纸质版合同
+                $data   = $this->generate($resident, ['type' => Contractmodel::TYPE_NORMAL]);
+//                $orderUnpaidCount   = $resident->orders()
+//                    ->whereIn('status', [Ordermodel::STATE_AUDITED, Ordermodel::STATE_PENDING, Ordermodel::STATE_CONFIRM])
+//                    ->count();
+//
+//                if (0 == $orderUnpaidCount) {
+//                    $resident->update(['status' => Residentmodel::STATE_NORMAL]);
+//                    $resident->room->update(['status' => Roommodel::STATE_RENT]);
+//                    $this->api_res(0);
+//                    return;
+//                }
+            }else{
+                $this->api_res(10016);
+                return;
+            }
+        }else{
+            if(empty($contract)){
+                //申请证书
+                $name       = $resident->name;
+                $phone      = $resident->phone;
+                $cardNumber = $resident->card_number;
+                $cardType   = $resident->card_type;
+                $customerCA = $this->getCustomerCA(compact('name', 'phone', 'cardNumber', 'cardType'));
+                //生成法大大合同
+                $data=$this->generate($resident, [
+                    'type' => Contractmodel::TYPE_FDD,
+                    'customer_id'   => $customerCA,
+                    ]);
+            }else{
+                $this->api_res(10016);
+                return;
+            }
+            //合同没归档就去签署页面
+//            if (Contractmodel::STATUS_ARCHIVED != $contract->status) {
+//                //$targetUrl = $this->getSignUrl($contract);
+//                $this->api_res(10016);
+//                return;
+//            }
+        }*/
+
+        $contract   = new Contractmodel();
+        //开始签约
+        try{
+            DB::beginTransaction();
+            //1,生成合同
+            $contract->store_id = $resident->store_id;
+            $contract->room_id  = $resident->room_id;
+            $contract->resident_id  = $resident->id;
+            $contract->uxid         = $resident->uxid;
+            //此用户id是fdd返回id而不是正常的customer_id
+            $contract->customer_id  = $resident->customer_id;
+            //$contract->fdd_customer_id  = $data['fdd_customer_id'];
+            $contract->type         = $data['type'];
+            $contract->employee_id  = $resident->employee_id;
+            $contract->contract_id  = $data['contract_id'];
+            $contract->doc_title    = $data['doc_title'];
+            $contract->download_url = $data['download_url'];
+            $contract->view_url     = $data['view_url'];
+            $contract->status       = $data['status'];
+//            $contract->sign_type       = Contractmodel::SIGN_NEW ;
+            $a  = $contract->save();
+            //2.生成订单
+            $this->load->model('ordermodel');
+            $b  = $this->ordermodel->firstCheckInOrders($resident, $room);
+
+            if($a && $b){
+                DB::commit();
+            }else{
+                DB::rollBack();
+                $this->api_res(1009);
+                return;
+            }
+            $this->api_res(0,['resident_id'=>$resident->id,'order_number'=>$b]);
+        }catch (Exception $e){
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
      * 生成签署合同的页面
      * */
         public function signContract(){
