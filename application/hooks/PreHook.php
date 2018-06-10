2e082f17e9912c10f22658482f9c87282721ddc0
zhangjunhu <401967974@qq.com>
2018-06-10 17:45:20 +0800

    注释掉prehook非POST拦截

--- application/hooks/PreHook.php
+++ application/hooks/PreHook.php
@@ -23,11 +23,11 @@ class PreHook {
             }
 
              //对于非post请求进行拦截报错
-             else if(!IS_POST){
-                 header('HTTP/1.1 403 Forbidden');
-                 echo 'access denied';
-                 exit;
-             }
+//             else if(!IS_POST){
+//                 header('HTTP/1.1 403 Forbidden');
+//                 echo 'access denied';
+//                 exit;
+//             }
 
 
              // 对token参数进行校验
