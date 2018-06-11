521eb44321140cb9b62693f5c8a563cfdf96f82c
zhangjunhu <401967974@qq.com>
2018-06-11 11:11:13 +0800

    test

--- application/controllers/resident/Order.php
+++ application/controllers/resident/Order.php
@@ -52,8 +52,8 @@ class Order extends MY_Controller
 
         $resident   = Residentmodel::with(['roomunion','orders'=>function($query){
             $query->whereIn('status',[Ordermodel::STATE_CONFIRM,Ordermodel::STATE_COMPLETED]);
-//        }])->where('customer_id',$this->user->id);
-        }])->where('customer_id',5373);
+        }])->where('customer_id',$this->user->id);
+//        }])->where('customer_id',5373);
         $orders  = $resident->get()->map(function($query){
             $query->count  = count($query->orders);
             $query->amount = $query->orders->sum('money');
