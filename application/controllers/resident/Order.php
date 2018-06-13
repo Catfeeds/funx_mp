334b27c57be6abb921617c986ba9d335ce7522e3
zhangjunhu <401967974@qq.com>
2018-06-13 20:18:08 +0800

    test

--- application/controllers/resident/Order.php
+++ application/controllers/resident/Order.php
@@ -28,11 +28,11 @@ class Order extends MY_Controller
 
         $resident   = Residentmodel::with(['roomunion','orders'=>function($query){
             $query->where('status',Ordermodel::STATE_PENDING);
-        }])->where('customer_id',9750);
+        }])->where('customer_id',$this->user->id);
 //        log_message('error','UNPAID-->'.$this->user->id);
-        echo $this->user->id;
+//        echo $this->user->id;
 
-        var_dump($resident->get()->toArray());exit;
+//        var_dump($resident->get()->toArray());exit;
 
 //        }])->where('customer_id',9604);
         $orders  = $resident->get()->map(function($query){
