99a6ce4ce6b486454dd4bfb2f9dded09cec847e1
chenkunkun <cooook@163.com>
2018-06-14 13:17:07 +0800

    '微信更新';

--- application/models/Billmodel.php
+++ application/models/Billmodel.php
@@ -9,4 +9,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Billmodel extends Basemodel
 {
     protected $table    = 'boss_bill';
+
+    protected $casts    = ['data'=>'array'];
+
+    public function roomunion()
+    {
+
+        return $this->belongsTo(Roomunionmodel::class,'room_id');
+    }
+
+    public function store()
+    {
+
+        return $this->belongsTo(Storemodel::class,'store_id');
+    }
+
+    public function resident()
+    {
+
+        return $this->belongsTo(Residentmodel::class,'resident_id');
+    }
+
+    public function employee()
+    {
+
+        return $this->belongsTo(Employeemodel::class,'employee_id');
+    }
+
+    /**
+     * 检索当日确定的账单的数量
+     */
+    public function ordersConfirmedToday()
+    {
+        return Billmodel::whereDate('updated_at', '=', date('Y-m-d'))
+            ->count();
+    }
 }
