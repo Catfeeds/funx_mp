8419ac2d2fd6682ab01fd4d4c99089cc87cd1709
ete <ete0918@163.com>
2018-07-03 08:43:11 +0800

    'reserve'

--- application/controllers/service/Reserve.php
+++ application/controllers/service/Reserve.php
@@ -55,7 +55,9 @@ class Reserve extends MY_Controller
             ->whereIn('status',['WAIT','BEGIN'])->get($filed)
             ->map(function ($item){
                 if (isset($item->room_type->images)){
+                    var_dump($item->room_type->images);
                     $item->room_type->images = $this->fullAliossUrl(json_decode($item->room_type->images,true),true);
+                    var_dump($item->room_type->images);
                 }
                 return $item;
             })->toArray();
