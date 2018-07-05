105ed9ab07e1259145051620bba6dd9e08b5fac8
ete <ete0918@163.com>
2018-07-05 11:51:52 +0800

    'order'

--- application/controllers/shop/Order.php
+++ application/controllers/shop/Order.php
@@ -40,7 +40,7 @@ class Order extends MY_Controller
         $this->load->model('residentmodel');
         $uxid = CURRENT_ID;
         $resident   = Residentmodel::where('customer_id',$uxid)->first();
-        $paid    = $resident->orders()->with('store','roomunion')
+        $paid    = $resident->orders()->store()->roomunion()
             ->whereIn('status',[Ordermodel::STATE_CONFIRM,Ordermodel::STATE_COMPLETED])
             ->orderBy('year','DESC')
             ->orderBy('month','DESC')
