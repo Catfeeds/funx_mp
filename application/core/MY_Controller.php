b786d9e935e7f20a0f236ddc2f4960d2c171324a
Chuanjian Wang <me@ckeyer.com>
2018-07-31 09:41:01 +0800

    fix http status

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,37 +9,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class MY_Controller extends CI_Controller {
     const GET_ACCESS_TOKEN = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=";
-    const GET_TICKET = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=";
-    public function __construct()
-    {
+    const GET_TICKET       = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=";
+    public function __construct() {
         parent::__construct();
         $this->output->set_content_type('application/json');
     }
 
     //API返回统一方法
-    public function api_res($code,$data = false)
-    {
+    public function api_res($code, $data = false) {
 
         $msg = $this->config->item('api_code')[$code];
         if ($data) {
             $this->output
                 ->set_content_type('application/json')
-                ->set_output(json_encode(array('rescode'=>$code,'resmsg'=>$msg,'data'=>$data)));
+                ->set_output(json_encode(array('rescode' => $code, 'resmsg' => $msg, 'data' => $data)));
         } else {
             $this->output
                 ->set_content_type('application/json')
-                ->set_output(json_encode(array('rescode'=>$code,'resmsg'=>$msg,'data'=>[])));
+                ->set_output(json_encode(array('rescode' => $code, 'resmsg' => $msg, 'data' => [])));
         }
         switch ($code) {
         case 0:
             break;
-        case 1002,1004,1005,10008:
+        case 1002:
+        case 1004:
+        case 1005:
+        case 10008:
             $this->set_status_header(400);
             break;
-        case 1001,1006,11005:
+        case 1001:
+        case 1006:
+        case 11005:
             $this->set_status_header(401);
             break;
-        case 1011,10011:
+        case 1011:
+        case 10011:
             $this->set_status_header(403);
             break;
         default:
@@ -49,38 +53,38 @@ class MY_Controller extends CI_Controller {
     }
 
     /**
-     *  $url 		curl请求的网址
-     *  $type		curl请求的方式，默认get
-     *  $res		curl 是否把返回的json数据转换成数组
-     *  $arr		curl post传递的数据
+     *  $url         curl请求的网址
+     *  $type        curl请求的方式，默认get
+     *  $res        curl 是否把返回的json数据转换成数组
+     *  $arr        curl post传递的数据
      */
-    public function httpCurl($url,$method='get',$res='',$arr=''){
+    public function httpCurl($url, $method = 'get', $res = '', $arr = '') {
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-        curl_setopt($ch,CURLOPT_HEADER,0);
-        curl_setopt($ch,CURLOPT_REFERER,0);
-        curl_setopt($ch,CURLOPT_HTTPHEADER,array("content-type: application/x-www-form-urlencoded;charset=UTF-8"));
-        if($method=='post'){
-            curl_setopt($ch,CURLOPT_POST,true);        // 开启post提交
-            curl_setopt($ch,CURLOPT_POSTFIELDS,$arr); //post 数据  http_build_query($data)
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_REFERER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("content-type: application/x-www-form-urlencoded;charset=UTF-8"));
+        if ($method == 'post') {
+            curl_setopt($ch, CURLOPT_POST, true); // 开启post提交
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $arr); //post 数据  http_build_query($data)
         }
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0); //CURLOPT_SSL_VERIFYHOST 设置为 1 是检查服务器SSL证书中是否存在一个公用名(common name)。
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); //CURLOPT_SSL_VERIFYHOST 设置为 1 是检查服务器SSL证书中是否存在一个公用名(common name)。
         $output = curl_exec($ch);
-        if(curl_errno($ch)){
+        if (curl_errno($ch)) {
             echo curl_error($ch);
             curl_close($ch);
             return false;
-        }else{
-            $encode = mb_detect_encoding($output, array("ASCII","UTF-8","GB2312","GBK","BIG5"));
-            if($encode !== "UTF-8"){
+        } else {
+            $encode = mb_detect_encoding($output, array("ASCII", "UTF-8", "GB2312", "GBK", "BIG5"));
+            if ($encode !== "UTF-8") {
                 $output = mb_convert_encoding($output, "UTF-8", $encode);
             }
-            if($res =='json'){
-                $output = json_decode($output,true);
+            if ($res == 'json') {
+                $output = json_decode($output, true);
             }
             curl_close($ch);
             return $output;
@@ -94,44 +98,36 @@ class MY_Controller extends CI_Controller {
      * config数组中包含 field 和config 两个类型
      * example $config=['filed'=>['a','b','c'],'config'=>[['field'=>'a'....],['field'=>'b'....]]]
      */
-    public function validationText($config,$data=[])
-    {
-        if(!empty($data)&&is_array($data))
-        {
+    public function validationText($config, $data = []) {
+        if (!empty($data) && is_array($data)) {
             $this->load->library('form_validation');
             $this->form_validation->set_data($data)->set_rules($config);
-            if(!$this->form_validation->run())
-            {
+            if (!$this->form_validation->run()) {
                 return false;
-            }else{
+            } else {
                 return true;
             }
         }
         $this->load->library('form_validation');
         $this->form_validation->set_rules($config);
-        if(!$this->form_validation->run())
-        {
+        if (!$this->form_validation->run()) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
-
-
 
     /**
      * @param array $fieldarr 传入验证字段的数组
      * @return string 返回验证表单的第一个错误信息方便 ajax返回
      */
-    function form_first_error($fieldarr = [])
-    {
-        if (FALSE === ($OBJ =& _get_validation_object()))
-        {
+    public function form_first_error($fieldarr = []) {
+        if (FALSE === ($OBJ = &_get_validation_object())) {
             return '';
         }
-        foreach ($fieldarr as $field){
-            if($OBJ->error($field)){
-                return $OBJ->error($field,NULL,NULL);
+        foreach ($fieldarr as $field) {
+            if ($OBJ->error($field)) {
+                return $OBJ->error($field, NULL, NULL);
             }
         }
     }
@@ -139,13 +135,13 @@ class MY_Controller extends CI_Controller {
     /**
      * 将alioss路径 拼接成完整的URL
      */
-    public function fullAliossUrl($oss_path,$bool=false){
-        if($bool==true&&is_array($oss_path)){
-            foreach ($oss_path as $path){
-                $full_url[]   = config_item('cdn_path').$path;
+    public function fullAliossUrl($oss_path, $bool = false) {
+        if ($bool == true && is_array($oss_path)) {
+            foreach ($oss_path as $path) {
+                $full_url[] = config_item('cdn_path') . $path;
             }
-        }else{
-            $full_url   = config_item('cdn_path').$oss_path;
+        } else {
+            $full_url = config_item('cdn_path') . $oss_path;
         }
         return $full_url;
     }
@@ -154,18 +150,18 @@ class MY_Controller extends CI_Controller {
      * 对上传的文件url拆解成alioss路径
      * true传入数组 对数组进行遍历拆解
      */
-    public function splitAliossUrl($full_path,$bool=false){
-        if($bool==true){
+    public function splitAliossUrl($full_path, $bool = false) {
+        if ($bool == true) {
             $alioss_path = [];
-            foreach ($full_path as $path){
-                $split   = substr($path,strlen(config_item('cdn_path')));
-                if(!$split){
+            foreach ($full_path as $path) {
+                $split = substr($path, strlen(config_item('cdn_path')));
+                if (!$split) {
                     throw new Exception('截取url失败');
                 }
-                $alioss_path[]=$split;
+                $alioss_path[] = $split;
             }
-        }else{
-            if(!$alioss_path    = substr($full_path,strlen(config_item('cdn_path')))){
+        } else {
+            if (!$alioss_path = substr($full_path, strlen(config_item('cdn_path')))) {
                 throw new Exception('截取url失败');
             }
         }
@@ -176,38 +172,37 @@ class MY_Controller extends CI_Controller {
      * 核对当前操作用户
      */
     /*  public function checkUser($uxid){
-          if($uxid!=CURRENT_ID){
-              return flase;
-  //            throw new Exception('核对当前操作用户异常');
-          }else{
-              return true;
-          }
-      }*/
+    if($uxid!=CURRENT_ID){
+    return flase;
+    //            throw new Exception('核对当前操作用户异常');
+    }else{
+    return true;
+    }
+    }*/
     /*
      * 获取access_token
-    */
-    public function get_access_token($appid,$secret){
-        $access_token=self::GET_ACCESS_TOKEN.$appid."&secret=".$secret;
-        $ticket = json_decode($this->httpCurl($access_token));
+     */
+    public function get_access_token($appid, $secret) {
+        $access_token = self::GET_ACCESS_TOKEN . $appid . "&secret=" . $secret;
+        $ticket       = json_decode($this->httpCurl($access_token));
         $jsapi_ticket = $this->get_ticket($ticket->access_token);
         return $jsapi_ticket;
     }
     /*
      *获取ticket
      * */
-    protected function get_ticket($access_token){
-        $jsapi_ticket = json_decode($this->httpCurl(self::GET_TICKET.$access_token."&type=jsapi"));
+    protected function get_ticket($access_token) {
+        $jsapi_ticket = json_decode($this->httpCurl(self::GET_TICKET . $access_token . "&type=jsapi"));
         return $jsapi_ticket->ticket;
     }
     /*
      * 获取随机字符串
      * */
-    public function random_str(){
-        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public function random_str() {
+        $chars    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         $password = "";
-        for ( $i = 0; $i < 10; $i++ )
-        {
-            $password .= $chars[ mt_rand(0, strlen($chars) - 1) ];
+        for ($i = 0; $i < 10; $i++) {
+            $password .= $chars[mt_rand(0, strlen($chars) - 1)];
         }
         return $password;
     }
